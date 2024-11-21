0de153c53252dbf3890dd6a80298044ba65d434f
Mariana <163425141+marianalmeid@users.noreply.github.com>
2024-11-21 09:47:43 -0300

    conexao

---
+++ postgre.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nome_do_banco',
+        'USER': 'seu_usuario',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
